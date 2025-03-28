81268b0a00d8529968d036be84aba197337927fc
Matthew Burket <m@tthewburket.com>
2025-03-27 21:13:06 -0500

    Adjust the output html to use a pool for multiprocessing

--- stigaview_static/html_output.py
+++ stigaview_static/html_output.py
@@ -1,6 +1,6 @@
+import multiprocessing
 import os.path
 import shutil
-from multiprocessing import Process
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -65,20 +65,8 @@ def write_products(products: list[models.Product], out_path: str) -> None:
     full_out_path = os.path.join(real_out, "index.html")
     os.makedirs(real_out, exist_ok=True)
     render_template("products.html", full_out_path, products=sorted(products))
-    processes = list()
-    for product in products:
-        p = Process(
-            target=render_product,
-            args=(
-                product,
-                real_out,
-            ),
-        )
-        p.start()
-        processes.append(p)
-
-    for process in processes:
-        process.join()
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+        pool.starmap(render_product, [(product, real_out) for product in products])
 
 
 def render_stig_index(products: list[models.Product], out_path: str) -> None:
